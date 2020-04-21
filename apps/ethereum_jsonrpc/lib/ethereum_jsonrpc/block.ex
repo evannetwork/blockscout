ce331e3b9ea3a1cbeb7a36a2142830bed62ae8c1
S3bb1 <sebastian.dechant@evan.team>
2020-04-21 20:24:33 +0200

    adjust css, fix emptysteps indexer

--- apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/block.ex
+++ apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/block.ex
@@ -452,7 +452,7 @@ defmodule EthereumJSONRPC.Block do
   # hash format
   defp entry_to_elixir({key, _} = entry)
        when key in ~w(author extraData hash logsBloom miner mixHash nonce parentHash receiptsRoot sealFields sha3Uncles
-                     signature stateRoot step transactionsRoot uncles bitcoinMergedMiningCoinbaseTransaction bitcoinMergedMiningHeader bitcoinMergedMiningMerkleProof hashForMergedMining),
+                     signature stateRoot step transactionsRoot uncles bitcoinMergedMiningCoinbaseTransaction bitcoinMergedMiningHeader bitcoinMergedMiningMerkleProof hashForMergedMining emptySteps),
        do: entry
 
   defp entry_to_elixir({"timestamp" = key, timestamp}) do
